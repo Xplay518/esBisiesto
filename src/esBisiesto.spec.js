fcafb8f4f182c084581bdef5aec4d671a08777bd
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-25 21:59:45 -0400

    3ra captura, toma en cuenta que el 5 no es anio bisiesto

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -2,9 +2,13 @@ import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
   it("El 4 deberia considerarse como un anio bisiesto", () => {
-    const result = esBisiesto(4);
-    expect(result).toEqual(true);
-    //expect(sumar(3, 2)).toEqual(5);
+    expect(esBisiesto(4)).toEqual(true);    //act assert
+  });
+
+  it("El 5 no deberia considerarse como un anio bisiesto", () => {
+    
+    expect(esBisiesto(5)).toEqual(false);
+    
   });
 });
 
