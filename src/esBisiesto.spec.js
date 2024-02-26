4619036240e4589d04933c0f9dac3876a70752ed
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-25 21:48:34 -0400

    Segunda captura, se separa en dos archivos

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,7 +1,10 @@
-//import sumar from "./sumador.js";
+import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-  it("", () => {
+  it("El 4 deberia considerarse como un anio bisiesto", () => {
+    const result = esBisiesto(4);
+    expect(result).toEqual(true);
     //expect(sumar(3, 2)).toEqual(5);
   });
 });
+
