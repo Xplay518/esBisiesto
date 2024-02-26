37b9004ee19987ca1bb10b5c01e6a22143eefc71
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-25 22:28:13 -0400

    2do commit, ya se tiene hasta el punto 2

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,5 +1,5 @@
 function esBisiesto(anio) {
-    if (anio % 400 === 0) {
+    if ((anio % 4 === 0 && anio % 100 !== 0) || anio % 400 === 0) {
         return true;
     }
     return false;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,32 +1,27 @@
 import esBisiesto from "./esBisiesto.js";
 
-/*
 describe("Es Bisiesto", () => {
-  it("El 4 deberia considerarse como un anio bisiesto", () => {
-    expect(esBisiesto(4)).toEqual(true);    //act assert
-  });
-
-  it("El 5 no deberia considerarse como un anio bisiesto", () => {
-    
-    expect(esBisiesto(5)).toEqual(false);
-    
-  });
-});
-  */
-
-
-
-describe("Es Bisiesto", () => {
-  it("Los años divisibles por 400 deberían considerarse como años bisiestos", () => {
+  it("Los años divisibles por 400 son años bisiestos", () => {
     expect(esBisiesto(2000)).toEqual(true);
     expect(esBisiesto(2400)).toEqual(true);
   });
 
-  it("Los años no divisibles por 400 no deberían considerarse como años bisiestos", () => {
-    expect(esBisiesto(2001)).toEqual(false);
+  it("Los años divisibles por 100 pero no por 400 no son años bisiestos", () => {
+    expect(esBisiesto(1700)).toEqual(false);
+    expect(esBisiesto(1800)).toEqual(false);
+    expect(esBisiesto(1900)).toEqual(false);
     expect(esBisiesto(2100)).toEqual(false);
   });
+
+  it("Los años divisibles por 4 pero no por 100 son años bisiestos", () => {
+    expect(esBisiesto(2004)).toEqual(true);
+    expect(esBisiesto(2008)).toEqual(true);
+    expect(esBisiesto(2012)).toEqual(true);
+  });
+
+  it("Los años no divisibles por 4 no son años bisiestos", () => {
+    expect(esBisiesto(2001)).toEqual(false);
+    expect(esBisiesto(2003)).toEqual(false);
+    expect(esBisiesto(2017)).toEqual(false);
+  });
 });
-
-
-
